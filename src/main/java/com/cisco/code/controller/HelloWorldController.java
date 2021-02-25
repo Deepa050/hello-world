cb63d33f0d1dd98270018eee8f4cb07af70085f9
Deepa Bharti <debharti@cisco.com>
2021-02-25 14:58:04 +0530

    change some stuff

--- src/main/java/com/cisco/code/controller/HelloWorldController.java
+++ src/main/java/com/cisco/code/controller/HelloWorldController.java
@@ -1,11 +1,6 @@
 package com.cisco.code.controller;
 
-
-import com.cisco.code.ResourceNotFoundException;
-import com.cisco.code.model.User;
-import com.cisco.code.repository.HelloWorldRespository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,8 +9,6 @@ import java.util.List;
 @RequestMapping("/")
 public class HelloWorldController {
 
-    @Autowired
-    private HelloWorldRespository helloWorldRespository;
 
     @RequestMapping
     String home() {
@@ -27,32 +20,4 @@ public class HelloWorldController {
         return "Welcome to Deepa's CI/CD world ";
     }
 
-    @GetMapping("/user")
-    public ResponseEntity<List<User>> getAllEmployees() {
-        return ResponseEntity.ok(helloWorldRespository.findAll());
-    }
-
-    @PostMapping("/user")
-    public User add(@RequestBody User user) {
-        return helloWorldRespository.save(user);
-    }
-
-
-    @GetMapping("/user/{id}")
-    public ResponseEntity<User> findEmployeeById(@PathVariable(value = "id") Integer employeeId) {
-        User employee = helloWorldRespository.findById(employeeId).orElseThrow(
-                () -> new ResourceNotFoundException("Employee not found" + employeeId));
-        return ResponseEntity.ok().body(employee);
-    }
-
-    @PutMapping("/user/{id}")
-    public ResponseEntity<User> updateEmployee(@PathVariable(value = "id") Integer employeeId,
-                                               @RequestBody User employeeDetails) {
-        User employee = helloWorldRespository.findById(employeeId)
-                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
-        employee.setName(employeeDetails.getName());
-        final User updatedEmployee = helloWorldRespository.save(employee);
-        return ResponseEntity.ok(updatedEmployee);
-
-    }
 }
